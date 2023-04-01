e0f69e84722a2360d41313c97a7c348953ef9872
oeseo <ali301194@me.com>
2023-04-01 20:44:18 +0300

    code according to PEP8

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,13 +1,18 @@
 from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
-from cats.views import CatViewSet
-
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register(r'mycats', LightCatViewSet)
+router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
